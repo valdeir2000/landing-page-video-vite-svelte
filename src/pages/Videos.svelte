504be9efaf9ab3854afba1ba1a959c00a3697bd1
Valdeir S <valdeirpsr@hotmail.com.br>
2023-03-11 15:19:22 -0300

    feat: adiciona página videos
    A página será responsável por carregar e exibir os vídeos para os usuários

---
+++ src/pages/Videos.svelte
@@ -0,0 +1,42 @@
+<script>
+  import Player from '../components/Player.svelte';
+  import Comments from '../components/Comments.svelte';
+</script>
+
+<div id="header">
+  <h1>Clique em play!</h1>
+  <p>Curta, comente, baixe!</p>
+  <div class="arrow-down"></div>
+</div>
+
+<!-- Player Here -->
+<Player />
+
+<Comments />
+
+<style>
+  #header {
+  margin: 0 auto;
+  text-align: center;
+  grid-column: auto/span 12;
+}
+
+#header h1 {
+  letter-spacing: 5px;
+  text-align: center;
+  text-transform: uppercase;
+}
+
+#header p {
+  font-weight: 200;
+}
+
+.arrow-down {
+  border-bottom: 1px solid #FFF;
+  border-left: 1px solid #FFF;
+  height: 29px;
+  margin: 2.5rem auto 0;
+  transform: rotate(-45deg);
+  width: 29px;
+}
+</style>
