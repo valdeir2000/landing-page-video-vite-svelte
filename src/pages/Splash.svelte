d7abf97bbeea8af78e913171983c1cf71467c965
Valdeir S <valdeirpsr@hotmail.com.br>
2023-03-11 22:24:20 -0300

    feat: adiciona a página splash
    A página informará um aviso sobre o conteúdo do site

---
+++ src/pages/Splash.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Swal from 'sweetalert2';
+  import '../assets/css/splash.css';
+
+  Swal.fire({
+    title: 'Atenção!',
+    html: import.meta.env.VITE_SPLASH_MESSAGE,
+    width: 300,
+    showConfirmButton: true,
+    confirmButtonText: 'Continuar',
+    confirmButtonColor: '#20c3c4',
+    showCloseButton: true,
+    customClass: {
+      confirmButton: 'w-full'
+    }
+  })
+  .then((result) => {
+    if (result.isConfirmed) window.location.hash = 'videos';
+    else window.location.href = import.meta.env.VITE_URL_REDIRECT;
+  })
+</script>
