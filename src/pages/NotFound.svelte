8be92be265fedde04eb555839d999dc2aff9b78a
Valdeir S <valdeirpsr@hotmail.com.br>
2023-03-12 02:07:07 -0300

    feat: adiciona a página NotFound - 404

---
+++ src/pages/NotFound.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { LottiePlayer } from '@lottiefiles/svelte-lottie-player';
+</script>
+
+<div>
+  <LottiePlayer
+    src="https://assets1.lottiefiles.com/packages/lf20_suhe7qtm.json"
+    autoplay
+    loop
+    background="transparent"
+    class="lottie-player"
+  />
+</div>
+
+<style>
+  div {
+    display: flex;
+    grid-column: 2 / span 10;
+    place-content: center;
+    place-items: center;
+  }
+
+  @media (min-width: 768px) {
+    div {
+      grid-column: 4 / span 6;
+    }
+  }
+
+  @media (min-width: 1024px) {
+    div {
+      grid-column: 5 / span 4;
+    }
+  }
+</style>
