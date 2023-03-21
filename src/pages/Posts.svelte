643168cb7ec610e03b7f934f4abf396144334081
Valdeir S <valdeirpsr@hotmail.com.br>
2023-03-21 17:33:05 -0300

    feat: adiciona modo leitura na página de posts

--- src/pages/Posts.svelte
+++ src/pages/Posts.svelte
@@ -1,22 +1,60 @@
-<script>
+<script lang="ts">
+  import { tick } from 'svelte';
+  import Comment from '../components/Comments.svelte';
   import posts from '../assets/json/posts.json';
+  /* import commentsDefault from '../assets/json/comments.json'; */
+
+  let readerMode = false;
+  let contentHeading: string;
+  let content: HTMLDivElement;
+
+  let readerPost = async (id: number) => {
+    readerMode = true;
+    await tick();
+
+    const post = posts.filter((post) => post.id === id).at(0);
+
+    fetch(`/posts/${id}.html`)
+      .then((res) => res.text())
+      .then((res) => {
+        contentHeading = post.title;
+        content.innerHTML = res;
+
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+        })
+      })
+  }
 </script>
 
-<div id="header">
-  <h1>Escolha e leia!</h1>
-  <p>Curta, comente, leia!</p>
-  <div class="arrow-down"></div>
-</div>
+{#if readerMode === true}
+<section class="container">
+  <h1>{ contentHeading }</h1>
+  <div class="content" bind:this={content}></div>
 
-<div class="wrapper">
-  {#each posts as post}
-  <article class="tale">
-    <img src={post.image} alt="Imagem de descrição do post">
-    <h1>{post.title}</h1>
-    <p>{post.description}</p>
-  </article>
-  {/each}
-</div>
+  <Comment />
+</section>
+{/if}
+
+<section id="list-posts">
+  <div id="header">
+    <h1>Escolha e leia!</h1>
+    <p>Curta, comente, leia!</p>
+    <div class="arrow-down"></div>
+  </div>
+  
+  <div class="wrapper">
+    {#each posts as post}
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <article class="tale" on:click={() => readerPost(post.id)}>
+      <img src={post.image} alt="Imagem de descrição do post">
+      <h1>{post.title}</h1>
+      <p>{post.description}</p>
+    </article>
+    {/each}
+  </div>
+</section>
 
 <style>
   #header {
@@ -45,10 +83,13 @@
   }
 
 
+  #list-posts {
+    grid-column: 2 / span 10;
+  }
+
   .wrapper {
     display: grid;
     gap: 16px;
-    grid-column: 2 / span 10;
     margin-top: 32px;
     width: 100%;
   }
@@ -91,5 +132,14 @@
       gap: 32px;
       grid-template-columns: repeat(3, 1fr);
     }
+  }
+
+  section.container {
+    backdrop-filter: blur(10px);
+    background: #ffffffdb;
+    color: #333;
+    grid-column: 1 / span 12;
+    margin: 0 8px;
+    padding: 16px;
   }
 </style>
